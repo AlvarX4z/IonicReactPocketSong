598f5e3f61c1599cd75d6eaf441f9c7f055b5fab
Alvaro de Francisco <alfrasan@gmail.com>
2020-11-24 09:06:26 +0100

    Dynamic List + Android Setup

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { 
-  IonContent, 
-  IonHeader, 
-  IonPage, 
-  IonTitle, 
+import React, { useEffect, useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
   IonToolbar,
   IonList,
   IonItem,
@@ -11,53 +11,65 @@ import {
   IonLabel
 } from '@ionic/react';
 import { musicalNote } from 'ionicons/icons';
+import { Plugins, FilesystemDirectory } from '@capacitor/core';
 import './Tab2.css';
 
+const { Filesystem } = Plugins;
+
 const Tab2: React.FC = () => {
+  
+  //crea una interface con los que te devuelva en el any 
+  const [song, setSong] = useState<string[]>([]);
+  
+  const readMusicDirectory = async () => {
+    try {
+      const list = await Filesystem.readdir({
+        path: '/',
+        directory: FilesystemDirectory.Documents
+      });
+      setSong(list.files);
+    } catch (e) { console.error('Unable to read dir', e); }
+  }
+  
+  useEffect(() => { readMusicDirectory(); }, []);
+
   return (
     <IonPage>
-
+  
       {/* Header */}
       <IonHeader>
         <IonToolbar>
           <IonTitle>Songs</IonTitle>
         </IonToolbar>
       </IonHeader>
-
+  
       {/* Songs Content */}
       <IonContent fullscreen>
-
+  
         {/* For iOS */}
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Songs</IonTitle>
           </IonToolbar>
         </IonHeader>
-
+  
         <IonList>
-
-          <IonItem className="songs-item" lines="full">
-            <IonIcon className="songs-item-icon" icon={musicalNote} size="large" slot="start" />
-            <IonLabel>
-              <h1>Song Name</h1> 
-              <h2>Song Artist</h2>
-            </IonLabel>
-          </IonItem>
-
-          <IonItem className="songs-item" lines="full">
-            <IonIcon className="songs-item-icon" icon={musicalNote} size="large" slot="start" /> 
-            <IonLabel>
-              <h1>Song Name</h1> 
-              <h2>Song Artist</h2>
-            </IonLabel>
-          </IonItem>
-
-        </IonList>
-
-      </IonContent>
-
+          {song.map((item) => {
+            return (
+              <IonItem className="songs-item" lines="full">
+                <IonIcon className="songs-item-icon" icon={musicalNote} size="large" slot="start" />
+                <IonLabel>
+                  <h1>{item}</h1>
+                  <h2>Unknown Artist</h2>
+                </IonLabel>
+              </IonItem>
+              )
+            })}
+          </IonList>
+  
+        </IonContent>
+  
     </IonPage>
   );
 };
-
 export default Tab2;
